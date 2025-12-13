ab4463bfa18db2bf4bc1843cc4d466f7b35b32c1
Vinicius <Viniciusr.camargo@gmail.com>
2025-12-13 12:49:52 -0300

    Aprendendo oo em JS

--- User.js
+++ User.js
@@ -1,6 +1,17 @@
 class User{
-    constructor(nome, email){
+    constructor(nome, email, nascimento, role, ativo = true){
         this.nome = nome;
         this.email = email;
+        this.nascimento = nascimento;
+        this.role = role || 'estudante';
+        this.ativo = ativo;
+    }
+    // Método para exibir informações do usuário
+    ExibirInfos(){
+        return `Nome: ${this.nome}, Email: ${this.email}, Nascimento: ${this.nascimento}`;
     }
 }
+
+const novoUsuario = new User('Vinicius','v@v.com','10/10/2000');
+console.log(novoUsuario);
+console.log(novoUsuario.ExibirInfos());
